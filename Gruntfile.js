9204a5617afa32a563954a54f325917cf015035d
Toby T Castro <tc6586@gmail.com>
2015-04-15 14:42:10 -0700

    heroku buildpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,8 @@ module.exports = function(grunt) {
 
     grunt.registerTask( 'run', [ 'hapi', 'watch' ]);
 
+    grunt.registerTask( 'heroku:production', ['default']);
+
     grunt.initConfig({
         browserify: {
             dist: {
